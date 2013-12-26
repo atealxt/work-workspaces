89425e58f706c30e8c923d758c92dc38d7c8dd76
Luis Ashurei <atealxt@gmail.com>
2013-12-26 14:15:46 +0800

    Create Problem1.java

---
+++ ProjectEuler/src/projecteuler/problem/Problem1.java
@@ -0,0 +1,27 @@
+package projecteuler.problem;
+
+import projecteuler.ProblemTemplate;
+
+public class Problem1 extends ProblemTemplate {
+
+	@Override
+	public String getTitle() {
+		return "Multiples of 3 and 5";
+	}
+
+	@Override
+	public String getURL() {
+		return "http://projecteuler.net/problem=1";
+	}
+
+	@Override
+	public String getResult() {
+		int sum = 0;
+		for (int i = 1; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+		return String.valueOf(sum);
+	}
+}
